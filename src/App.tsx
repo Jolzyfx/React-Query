ab48d756a2677726e5bf1ecd1177896f08c2fc11
JolzyFx Codes <jolzyanth@gmail.com>
2023-05-11 10:10:42 +0100

    tquery

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import PostList from './React Query/PostList'
+import TodoForm from './React Query/TodoForm'
+import TodoList from './React Query/TodoList'
+
+
+function App() {
+  return <>
+    <TodoForm/>
+    <TodoList/>
+  </>
+
+       
+}
+
+export default App
