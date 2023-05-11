a79b5f19a703b2cdc58df4f7e19aea1b6ddba1ab
JolzyFx Codes <jolzyanth@gmail.com>
2023-05-11 10:14:05 +0100

    query

--- src/React Query/hooks/useAddTodo.ts
+++ src/React Query/hooks/useAddTodo.ts
@@ -1,46 +1,40 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-
 import { CACHE_KEY_TODOS } from "../constants";
 import todoService, { Todo } from "../services/todoService";
 
-
-
-
 interface AddTodoContext {
-  previousTodos: Todo[]
+  previousTodos: Todo[];
 }
 
-
-const useAddTodo = (onAdd: ()=> void) => {
+const useAddTodo = (onAdd: () => void) => {
   const queryClient = useQueryClient();
-  return  useMutation<Todo, Error, Todo, AddTodoContext>({
+  return useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: todoService.post,
 
     onMutate: (newTodo: Todo) => {
-    const previousTodos =  queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || []
+      const previousTodos =
+        queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
 
       queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos = []) => [
         newTodo,
         ...todos,
       ]);
 
+      onAdd();
 
-      onAdd()
-      
-
-      return { previousTodos}
+      return { previousTodos };
     },
     onSuccess: (savedTodo, newTodo) => {
       queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos) =>
         todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
       );
     },
-    onError: (_error, _newTodo, context ) => {
-      if (!context) return 
-       queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, context.previousTodos)
-    }
+    onError: (_error, _newTodo, context) => {
+      if (!context) return;
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, context.previousTodos);
+    },
   });
-}
+};
 
-export default useAddTodo
+export default useAddTodo;
