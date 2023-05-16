15101c435a280deeac86ee53e8895d94db1aba07
JolzyFx Codes <jolzyanth@gmail.com>
2023-05-16 10:26:10 +0100

    next auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-
-import { useReducer } from 'react';
-import './App.css';
-import HomePage from './state management/HomePage';
-import NavBar from './state management/NavBar';
-import TasksContext from './state management/contexts/tasksContext';
-import tasksReducer from './state management/reducers/tasksReducer';
-
+import { useReducer } from "react";
+import "./App.css";
+import HomePage from "./state management/HomePage";
+import NavBar from "./state management/NavBar";
+import TasksContext from "./state management/contexts/tasksContext";
+import tasksReducer from "./state management/reducers/tasksReducer";
+import authReducer from "./state management/reducers/authReducer";
+import AuthContext from "./state management/contexts/authContext";
 
 function App() {
-  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
+  const [user, authDispatch] = useReducer(authReducer, "");
+
+  return (
+  <div>
+    <AuthContext.Provider value={{user, dispatch: authDispatch}}>
     
-  return  (
-
-  <TasksContext.Provider value={{tasks, dispatch}}>
-  <NavBar/>
-    <HomePage/>
-  </TasksContext.Provider>
    
-
-  
-  )
-       
+      <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
+        <NavBar />
+        <HomePage />
+      </TasksContext.Provider>
+      </AuthContext.Provider>  
+  </div>
+  );
 }
 
-export default App
+export default App;
