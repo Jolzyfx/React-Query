47900400e759c9b793663e468fd514d1597feb77
JolzyFx Codes <jolzyanth@gmail.com>
2023-05-16 14:44:09 +0100

    custon hook for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import { useReducer } from "react";
 import "./App.css";
 import AuthProvider from "./state management/AuthProvider";
 import HomePage from "./state management/HomePage";
 import NavBar from "./state management/NavBar";
-import TasksContext from "./state management/contexts/tasksContext";
-import tasksReducer from "./state management/reducers/tasksReducer";
+import TaskProvider from "./state management/TaskProvider";
 
 function App() {
-  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-  
-
   return (
-  <div>
-    <AuthProvider>
-    
-   
-      <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
+    <div>
+      <AuthProvider>
+        <TaskProvider>
         <NavBar />
         <HomePage />
-      </TasksContext.Provider>
-      </AuthProvider>  
-  </div>
+        </TaskProvider>
+      </AuthProvider>
+    </div>
   );
 }
 
