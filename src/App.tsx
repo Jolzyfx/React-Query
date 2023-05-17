e0329a95ad48f64e9af21cb5dfe23d42b3367ceb
JolzyFx Codes <jolzyanth@gmail.com>
2023-05-17 15:51:45 +0100

    Refactor adding Zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import AuthProvider from "./state management/auth/AuthProvider";
+
+import Counter from "./state management/counter/Counter";
 import HomePage from "./state management/HomePage";
 import NavBar from "./state management/NavBar";
 import TaskProvider from "./state management/Tasks/TaskProvider";
@@ -7,12 +8,13 @@ import TaskProvider from "./state management/Tasks/TaskProvider";
 function App() {
   return (
     <div>
-      <AuthProvider>
+     
         <TaskProvider>
+          <Counter/>
           <NavBar />
           <HomePage />
         </TaskProvider>
-      </AuthProvider>
+     
     </div>
   );
 }
