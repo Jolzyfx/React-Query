1d7d84694302df7a09b56e41a58e3d0bf77adc33
JolzyFx Codes <jolzyanth@gmail.com>
2023-05-19 20:44:23 +0100

    Nested Routes

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,23 +1,22 @@
-import  { createBrowserRouter } from "react-router-dom"
-import HomePage from "./HomePage"
-import UserListPage from "./UserListPage"
-import ContactPage from "./ContactPage"
-import UserDetailPage from "./UserDetailPage"
-import Layout from "./Layout"
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./HomePage";
+import Layout from "./Layout";
+import UserDetail from "./UserDetail";
+import UsersPage from "./UsersPage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element:<Layout/>,
+    path: "/",
+    element: <Layout />,
     children: [
-      {path: '', element: <HomePage/>},
-      {path: 'users', element: <UserListPage/>},
-      {path: 'contact', element: <ContactPage/>},
-      {path: 'users/:id', element: <UserDetailPage/>}
-    
-    ]
-  }
- 
-])
+      { path: "", element: <HomePage /> },
+      {
+        path: "users",
+        element: <UsersPage />,
+        children: [{ path: ":id", element: <UserDetail /> }],
+      },
+    ],
+  },
+]);
 
-export default router
+export default router;
